f3516894fa2a318238a0b772cac5b3c1ae1f4ce3
Greg Cowan <cowang4@yahoo.com>
2016-11-12 18:31:59 -0500

    ignored cache and drew all circles

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from graphics import *
 
 def main():
     win = GraphWin("Connect L&G", 940, 810) # x, y
-    c = Circle(Point(80,80), 50) # Point(x,y), radius
-    c2 = Circle(Point(210,80), 50)
-    c.draw(win)
-    c2.draw(win)
+    #c = Circle(Point(80,80), 50) # Point(x,y), radius
+    circles = [ [ Circle(Point(80+x*130, 80+y*130), 50) for y in range(6)] for x in range(7)]
+    for col in circles:
+        for circle in col:
+            circle.draw(win)
+
     print(win.getMouse()) # Pause to view result
     win.close()    # Close window when done
 
